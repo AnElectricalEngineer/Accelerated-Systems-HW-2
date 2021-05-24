9fe794f016045248dc30d476083476984d0cc42e
RoiAz <roiazarzar123@gmail.com>
2021-05-24 12:52:20 +0300

    constarins

--- ex2.cu
+++ ex2.cu
@@ -145,15 +145,83 @@ std::unique_ptr<image_processing_server> create_streams_server()
     return std::make_unique<streams_server>();
 }
 
+//////////////////////////***************************///////////////////////////////
+#define TASKS_PER_QUEUE 16
+#define SHARED_MEM_PER_THREAD_BLOCK 1500
+#define REG_PER_THREAD 32
+
+class gpu_task{
+public:
+	int    _img_id;
+	uchar *_img_in;
+	uchar *_img_out;
+
+	__host__ __device__ gpu_task(int img_id, uchar *img_in, uchar *img_out) :
+	                	_img_id(img_id), _img_in(img_in), _img_out(img_out){}
+};
+
+
+typedef cuda::atomic<size_t> cpu_gpu_atomic;
+
+class queue{
+	gpu_task _tasks[TASKS_PER_QUEUE];
+	cpu_gpu_atomic _head, _tail;
+public:
+
+	queue(): _head(0), _tail(0){}
+
+	__host__ __device__ void push(gpu_task task){
+		int tail = _tail.load(cuda::memory_order_relaxed);
+		while ((tail - _head.load(cuda::memory_order_acquire)) == TASKS_PER_QUEUE);
+		_tasks[_tail % TASKS_PER_QUEUE] = task;
+		_tail.store(tail+1, cuda::memory_order_release);
+	}
+
+	__host__ __device__ gpu_task pop(){
+		int head = _head.load(cuda::memory_order_relaxed);
+		while (head == _tail.load(cuda::memory_order_acquire);
+		gpu_task task = _tasks[ head % TASKS_PER_QUEUE];
+		_head.store(head+1, cuda::memory_order_release);
+		return task;
+	}
+};
+
 class queue_server : public image_processing_server
 {
 private:
     // TODO define queue server context (memory buffers, etc...)
+	queue** cpu_to_gpu;
+	queue** gpu_to_cpu;
+	int num_of_blocks;
+
+
 public:
     queue_server(int threads)
     {
         // TODO initialize host state
         // TODO launch GPU producer-consumer kernel with given number of threads
+    	cudaDeviceProp gpu_prop;
+    	cudaGetDeviceProperties(&gpu_prop, 0);
+    	size_t max_threads_per_core = gpu_prop.maxThreadsPerMultiProcessor;
+    	size_t num_of_cores =  gpu_prop.multiProcessorCount;
+    	size_t num_of_blocks = (num_of_cores * max_threads_per_core) /  threads;
+    	size_t shared_mem_per_core = gpu_prop.sharedMemPerMultiprocessor;
+    	size_t regs_per_core = gpu_prop.regsPerMultiprocessor;
+    	size_t max_blocks_per_core = gpu_prop.maxBlockPerMultiProcessor;
+
+    	if(num_of_blocks >  num_of_cores * max_blocks_per_core)
+    		num_of_blocks = num_of_cores * max_blocks_per_core;
+
+    	if (num_of_blocks > num_of_cores * (size_t)(shared_mem_per_core / SHARED_MEM_PER_THREAD_BLOCK))
+    		num_of_blocks = num_of_cores * (size_t)(shared_mem_per_core / SHARED_MEM_PER_THREAD_BLOCK);
+
+    	if (num_of_blocks > num_of_cores * (size_t)(regs_per_core / (threads * REG_PER_THREAD)))
+    		num_of_blocks = num_of_cores * (size_t)(regs_per_core / (threads * REG_PER_THREAD));
+
+
+
+
+
     }
 
     ~queue_server() override
